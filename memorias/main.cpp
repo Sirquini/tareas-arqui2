f48849dc700f7bd0d0df532e0f8df35e5c0c9df3
Sirquini <sirquini@hotmail.com>
2014-11-04 00:10:43 -0500

    adiciones a la cache

--- memorias/main.cpp
+++ memorias/main.cpp
@@ -3,24 +3,69 @@
 #include <iostream>
 #include <vector>
 
+/* 
+ * Utilidad para saber el numero minimo de bits para representar
+ * n valores.
+ */
+int bits_para(int n)
+{
+    int i, acumulador;
+	i = 1;
+    acumulador = 2;
+	while (acumulador < n)
+	{
+		++i;
+		acumulador = acumulador * 2;
+	}
+	return i;
+}
+
+
 typedef std::vector< std::vector< std::vector<int> > > tipo_mem;
 typedef std::vector< std::vector<int> > tipo_vias;
 
+/*
+ * En las operaciones de lectura y escritura se retorna un vector con tres enteros,
+ * donde el primer entero indica si hay un hit - 1 o un miss - 0,
+ * el segundo enteri indica si hay escritura en memoria principal,
+ * el tercer entero es el dato a escribir en memoria principal (de haber uno).
+ */
 class cache
 {
 public:
 	int vias,
 		bloques,
-		bloque_size;
-	std::vector< std::vector< std::vector<int> > > mem;
+		bloque_size,
+		posiciones,
+		boffset,
+		bdir;
+	tipo_mem mem;
+	tipo_vias LFU_counter;
 
-	cache(a_vias, a_bloques, a_bloque_size)
+
+	cache(int a_vias, int a_bloques, int a_bloque_size)
 	{
 		vias = a_vias;
 		bloques = a_bloques;
 		bloque_size = a_bloque_size;
+		posiciones = bloques/vias;
+		boffset = bits_para(bloque_size);
+		bdir = bits_para(posiciones);
 		mem = tipo_mem (bloques/vias, tipo_vias (vias, std::vector<int> (2+bloque_size,0)));
+		LFU_counter = tipo_vias (bloques/vias, std::vector<int> (vias,0));
 	}
+
+	std::vector<int> read_cache(int direccion)
+	{
+		std::vector<int> result (3, 0);
+
+	}
+
+	std::vector<int> write_cache(int direccion, int dato)
+	{
+		std::vector<int> result (3, 0);
+	}
+
 	~cache();
 };
 
