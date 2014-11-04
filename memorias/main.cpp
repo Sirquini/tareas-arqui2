bed9066ad74ae5d456296561005babdf480ab828
Sirquini <sirquini@hotmail.com>
2014-11-03 22:52:38 -0500

    primer modelado de cache

--- memorias/main.cpp
+++ memorias/main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <vector>
+
+typedef std::vector< std::vector< std::vector<int> > > tipo_mem;
+typedef std::vector< std::vector<int> > tipo_vias;
+
+class cache
+{
+public:
+	int vias,
+		bloques,
+		bloque_size;
+	std::vector< std::vector< std::vector<int> > > mem;
+
+	cache(a_vias, a_bloques, a_bloque_size)
+	{
+		vias = a_vias;
+		bloques = a_bloques;
+		bloque_size = a_bloque_size;
+		mem = tipo_mem (bloques/vias, tipo_vias (vias, std::vector<int> (2+bloque_size,0)));
+	}
+	~cache();
+};
+
 
 
 int main(int argc, char const *argv[])
