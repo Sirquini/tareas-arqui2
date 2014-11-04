53953c1d68483e96fa3da51ec4bb3eaa853741f4
Sirquini <sirquini@hotmail.com>
2014-11-03 22:04:07 -0500

    Algunas variables

--- memorias/main.cpp
+++ memorias/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+
 int main(int argc, char const *argv[])
 {
+	int bloques,
+		paginas,
+		vias,
+		accesos,
+		bloque_size,
+		pagina_size
+		fallos_pagina,
+		fallos_cache;
+
+
+
 	std::cout << "test memorias" << std::endl;
 	return 0;
 }
